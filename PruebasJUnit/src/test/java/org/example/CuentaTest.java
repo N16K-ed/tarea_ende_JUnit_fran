f57a87744f0b1504e0b8f73a90b3409ce0d8f39c
@w@ <fsannicceb@educacion.navarra.es>
2025-04-10 20:47:51 +0200

    creacion del proyecto
    con maven, junit y la clase de pruebas vacia

---
+++ PruebasJUnit/src/test/java/org/example/CuentaTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @Test
+    void getNumero() {
+    }
+
+    @Test
+    void getSaldo() {
+    }
+
+    @Test
+    void setNumero() {
+    }
+
+    @Test
+    void setSaldo() {
+    }
+
+    @Test
+    void ingresarDinero() {
+    }
+
+    @Test
+    void extraerDinero() {
+    }
+
+    @Test
+    void mostrarCuenta() {
+    }
+}
